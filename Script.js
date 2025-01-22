8896dcfb05be9484917bfb5202d11b558f53eff1
Anthony Edward <Anthony_ehab@hotmail.com>
2025-01-22 18:32:50 +0200

    added more non-functional buttons idk why

--- Script.js
+++ Script.js
@@ -7,23 +7,38 @@ document.addEventListener('scroll', () =>{
     }
 })
 
-var copybutton = document.getElementById("copy-button");
-var copybutton1 = document.getElementById("copy-button1");
-var copybutton2 = document.getElementById("copy-button2");
-var copybutton3 = document.getElementById("copy-button3");
+document.addEventListener("DOMContentLoaded", function() {
+    var copybutton = document.getElementById("copy-button");
+    var copybutton1 = document.getElementById("copy-button1");
+    var copybutton2 = document.getElementById("copy-button2");
+    var copybutton3 = document.getElementById("copy-button3");
+    var copybutton4 = document.getElementById("copy-button4");
+    var copybutton5 = document.getElementById("copy-button5");
+    var copybutton6 = document.getElementById("copy-button6");
 
-copybutton.onclick = function(){
-    navigator.clipboard.writeText("eng.anthonyedward@gmail.com");
-}
+    copybutton.onclick = function(){
+        navigator.clipboard.writeText("eng.anthonyedward@gmail.com");
+    }
 
-copybutton1.onclick = function(){
-    navigator.clipboard.writeText("+201222409650");
-}
+    copybutton1.onclick = function(){
+        navigator.clipboard.writeText("+201222409650");
+    }
 
-copybutton2.onclick = function(){
-    navigator.clipboard.writeText("Anthony Ehab Hany Edward Farid");
-}
+    copybutton2.onclick = function(){
+        navigator.clipboard.writeText("Anthony Ehab Hany Edward Farid");
+    }
 
-copybutton3.onclick = function(){
-    navigator.clipboard.writeText("Swissquote Bank Ltd");
-}
+    copybutton3.onclick = function(){
+        navigator.clipboard.writeText("Swissquote Bank Ltd");
+    }
+
+    copybutton4.onclick = function(){
+        navigator.clipboard.writeText("CH4708781000259461000");
+    }
+    copybutton5.onclick = function(){
+        navigator.clipboard.writeText("SWQBCHZZXXX");
+    }
+    copybutton6.onclick = function(){
+        navigator.clipboard.writeText("8781");
+    }
+});
